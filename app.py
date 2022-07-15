bcb3407fe4080208404283c7d238da32ea87114b
palbl4 <pave1b@mail.ru>
2022-07-15 15:43:45 +0300

    Main version

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///item.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    male_female = db.Column(db.String(10), nullable=False)
+    title = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    img = db.Column(db.Text)
+
+    def __repr__(self):
+        return self.title
+
+
 
 
 @app.route('/')
@@ -11,17 +32,21 @@ def index():
 
 @app.route('/mens-shoes')
 def mens_shoes():
-    return render_template("mens-shoes.html")
+    items = Item.query.order_by(Item.title, Item.male_female, Item.description, Item.img).all()
+    return render_template("mens-shoes.html", data=items)
 
 
 @app.route('/female-shoes')
 def female_shoes():
-    return render_template("female-shoes.html")
+    items = Item.query.all()
+    return render_template("female-shoes.html", data=items)
 
 
 @app.route('/children-shoes')
 def children_shoes():
-    return render_template("children-shoes.html")
+    item = Item.query.all()
+    return render_template("children-shoes.html", data=item)
+
 
 
 if __name__ == "__main__":
